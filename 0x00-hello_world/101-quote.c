9bc75763db1028b99f10e7caa0056c951c99af21
mazijason <chukwukachisom007@gmail.com>
2022-09-08 07:21:44 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
  * main - Entry point
  *
@@ -8,7 +8,6 @@
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 
 	return (1);
 }
