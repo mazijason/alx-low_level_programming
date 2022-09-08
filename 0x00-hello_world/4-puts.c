f95d789ecbb33cf81f4d7694e55ffa7c3a569864
mazijason <chukwukachisom007@gmail.com>
2022-09-08 05:08:14 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,18 +2,15 @@
 
 /**
  *
- *  * Description: main - Prints out a string to stdout.
+ * main - Entry point
  *
- *   * Return: 0 if success.
- *
- *    */
-
+ *Return: Always 0 (Success)
+ */
 int main(void)
-
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+			return (0);
 
 }
