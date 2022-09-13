71d24891e313d2d3712d74bb1ea4bf9729ccd896
mazijason <chukwukachisom007@gmail.com>
2022-09-13 04:55:54 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,30 @@
-#include "main.h"
+#include <unistd.h>
 
-
-/**
- * main - Entry point
- *
- * Return: nothing, return void
- */
-
-int main(void)
+int _putchar(char c)
 {
-	char holberton[] = "_putchar";
-	int msg;
-	for (msg = 0; msg <= 8; msg++)
-	{
-		_putchar(_putchar[msg]);
-	}
-	_putchar('\n');
-	return (0);
+	return (write(1, &c, 1));
 }
+int main ()
+{
+
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+
+return 0;
+
+
+}
+
+
+
+
+
 
