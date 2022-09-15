08c9f5d69c50871fcf37776575d3f346fdaeddc7
mazijason <chukwukachisom007@gmail.com>
2022-09-15 04:26:43 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+void print_numbers(void);
+void print_most_numbers(void);
