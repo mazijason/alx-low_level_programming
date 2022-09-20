7152743a7a7110930639fa1ff8f4ee27dda0860a
mazijason <chukwukachisom007@gmail.com>
2022-09-20 01:17:31 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
- * _strlen - length of a string
- * @s: input char
- * Return: length of a string
+ *  _strlen - returns length of a string
+ *  @s: character to check
+ *  Return: 0;
  */
 int _strlen(char *s)
 {
 	int l = 0;
 
-	while (*s != '\0')
+	while (*(s + i) != '\0')
 		i++;
-
 	return (l);
 }
