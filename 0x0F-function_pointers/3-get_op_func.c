713703bb6414a799b414ae4afd96d17a3cd6e339
mazijason <chukwukachisom007@gmail.com>
2022-10-12 08:07:48 -0700

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,24 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
+#include "3-calc.h"
+
 /**
  * get_op_func - selects function to perform operation
+ * 		 the operation asked by the user.
  * @s: operator
+ *
  * Return: pointer to func that correspond to operator
+ * 	   to the operator given as a parameter.
  */
 int (*get_op_func(char *s))(int, int)
 {
-	int (*ptr_fun)(int, int);
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL},
+	};
 
-	ptr_fun = *s == '+' ? op_add :
-*s == '-' ? op_sub :
-*s == '*' ? op_mul :
-*s == '/' ? op_div :
-*s == '%' ? op_mod : NULL;
-	if (!ptr_fun)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	return (ptr_fun);
+	int i = 0;
+
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+
+	return (ops[i].f);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,4 +1,6 @@
-include <stdio.h>
+#include "3-calc.h"
+
+#include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
 /**
